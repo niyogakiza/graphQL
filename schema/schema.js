575a2a7c95685324adf275dcf58552d75082a664
Aimable <niyoai@yahoo.fr>
2018-04-17 18:07:51 +0100

    first commit

---
+++ schema/schema.js
@@ -0,0 +1,36 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+
+const users = [
+    { id: '23', firstName: 'Bill', age: 20 },
+    { id: '47', firstName: 'Samantha', age: 30 }
+];
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields:{
+        id: {type: GraphQLString },
+        firstName:{type: GraphQLString } ,
+        age: {type: GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        user:{
+            type: UserType,
+            args: { id: {type: GraphQLString }},
+            resolve(parentValue, args){
+                return _.find(users, { id: args.id });
+
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
